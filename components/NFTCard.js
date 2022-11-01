7fb4d14f7579756faa38b6d792f1c720db0c228e
Abdul-Fattah <adeoyin2828@gmail.com>
2022-11-01 18:51:46 +0100

    done

---
+++ components/NFTCard.js
@@ -0,0 +1,72 @@
+import { View, Image, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+
+import { COLORS, SIZES, SHADOWS, assets} from '../constants'
+import { CircleButton,RectButton } from './Button'
+import {SubInfo, EthPrice, NFTTitle} from './SubInfo'
+
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation()
+  return (
+    <View style={{
+       backgroundColor: COLORS.white,
+       borderRadius: SIZES.font,
+       marginBottom: SIZES.extraLarge,
+       margin: SIZES.base,
+       ...SHADOWS.dark
+    }}>
+      <View style={{ width: '100%', height: 250 }}>
+        <Image
+         source={data.image}
+         resizeMode="cover"
+         style={{
+           width: "100%",
+           height: "100%",
+           borderTopLeftRadius: SIZES.font,
+           borderTopRightRadius: SIZES.font
+        }}
+        />
+      </View> 
+
+      <CircleButton 
+       right={10}
+       top={10}
+       imgUrl={assets.heart}c
+      />
+      {/* <Text>NFT Card</Text> */}
+      <SubInfo/>
+
+        <View style={{
+          width: '100%',
+          padding: SIZES.font
+        }}>
+          <NFTTitle 
+          title={data.name} 
+          subTitle={data.creator} 
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+          />
+        </View>
+
+<View style={{
+  margin: SIZES.font,
+  flexDirection: 'row',
+  justifyContent: "space-between",
+  alignItems: 'center'
+}}>
+<EthPrice price={data.price}/>
+<RectButton 
+ minWidth={120}
+ fontSize={SIZES.font}
+//  handlePress={() => navigation.navigate("Details", { data })}
+handlePress={() => navigation.navigate("Details", { data })}
+/>
+</View>
+
+    </View>
+  )
+}
+
+export default NFTCard
